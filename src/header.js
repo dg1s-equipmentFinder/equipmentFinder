6316aa9e62f219bcd89adbe534bfe1dfc72c7c81
RedBeet <ohmingun9@gmail.com>
2023-02-15 20:24:51 +0900

    Fix: encode link to ascii

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ import {OptionButton} from "./components/App"
 
 function ToSearchLink(){
     let keyword = document.getElementById('searchText').value;
-    window.location.replace(`/search/${keyword}`)
+    window.location.replace(`/search/${encodeURIComponent(keyword)}`)
 }
 
 function Form(){
