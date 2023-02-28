f47a8b41497d7ffd7f3cf3271b4b61c172d08e67
RedBeet <ohmingun9@gmail.com>
2023-02-28 23:23:38 +0900

    Improve: 로딩 페이지 디자인 수정

--- src/routes/start_page.js
+++ src/routes/start_page.js
@@ -1,6 +1,8 @@
 import '../App.css';
 import Header from "../header"
 import Footer from '../footer';
+import loading from '../spinner.svg'
+import dg1sico from '../dg1s_logo.png'
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom';
@@ -10,14 +12,15 @@ function StartPage(){
     const [time, setTime] = useState(true);
 
     useEffect(() =>{
-        let timer = setTimeout(() => {setTime(!time)}, 2000)
+        // let timer = setTimeout(() => {setTime(!time)}, 2000)
     })
 
     if (time === false){
         navigate('/map')
     }
     return <div id="startPage">
-        하잉
+        <div id="loadingMsg"><img id='dg1s_logoimg' alt="DG1Sico" src={dg1sico} /><span id="loadingTxt">대구일과학고등학교 기자재 지도</span></div>
+        <img src={loading} alt="loading spiner"></img>
     </div>
 }
 
