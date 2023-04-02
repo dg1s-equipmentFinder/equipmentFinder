8812efc3930f343255aee59348d1164d34762121
juxxnee <123970315+juxxnee@users.noreply.github.com>
2023-04-02 14:53:02 +0900

    Create convertCsvToJson(key).py

---
+++ src/data/convertCsvToJson(key).py
@@ -0,0 +1,23 @@
+#-*-coding:cp949-*-
+import json
+import pandas as pd
+
+
+datas = pd.read_csv('C:/users/Owner/Documents/GitHub/equipmentFinder/src/data/data.csv', encoding='cp949')
+datas=datas.values.tolist()
+
+print(datas[0][0])
+
+productdict={}
+for data in datas:
+    id=data[0]
+    searchtitle=data[1]
+    roomtitle=data[2]
+    productitle=data[3]
+    
+    productdict[id]=searchtitle
+   
+print(productdict)
+file_path="./src/data/roomdata_sample_key.json"
+with open(file_path, 'w', encoding='UTF-8') as outfile:
+    json.dump(productdict, outfile, ensure_ascii=False)
